79a17f4c7d7c2db7639737c153452c685a395d6d
Yusuf <109858565+yusufunity@users.noreply.github.com>
2022-08-20 14:33:21 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'baza2',
+        'USER': 'yusufuser',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
